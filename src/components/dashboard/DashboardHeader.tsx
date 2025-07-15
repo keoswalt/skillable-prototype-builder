7fcee129c8667d11cc0c62e48390f2b4f260a57d
Kim Oswalt <130675196+keoswalt@users.noreply.github.com>
2025-07-15 12:32:57 -0500

    Replace lab advisor chip with button

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -2,7 +2,7 @@
  * Dashboard Header Component
  *************************/
 
-import { Chip } from '@/components/info/Chip';
+import { Button } from '@/components/buttons/Button';
 import { SplitButton } from '@/components/buttons/SplitButton';
 import { Icon, Icons } from '@/components/Icon';
 import { MenuItem } from '@/config/navigation';
@@ -17,13 +17,13 @@ export function DashboardHeader({ findMenuItems, createMenuItems }: DashboardHea
     <section className="flex flex-row justify-between items-center py-6">
       <div className="flex items-center gap-3">
         <h1 className="font-headline text-heading-md">Welcome back, Kim.</h1>
-        <Chip 
+        <Button 
           size="small" 
           variant="warning" 
           onClick={() => alert('Opens lab advisor home page')}
         >
-          8 Lab Advisor Recommendations
-        </Chip>
+          View Recommendations (8)
+        </Button>
       </div>
       
       <div className="flex items-center gap-2">
