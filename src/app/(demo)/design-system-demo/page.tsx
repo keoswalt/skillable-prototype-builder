3a9928a0dd355f49e3ca39d5e91ab905cf997599
Kim Oswalt <130675196+keoswalt@users.noreply.github.com>
2025-07-16 08:57:52 -0500

    Add dividers and larger headings to design syystem demo page

--- src/app/(demo)/design-system-demo/page.tsx
+++ src/app/(demo)/design-system-demo/page.tsx
@@ -20,113 +20,154 @@ import FormattingToolbarExample from '@/components/editor/FormattingToolbarExamp
 import AdvancedMenuExample from '@/components/menu/AdvancedMenuExample';
 import FilterMenuExample from '@/components/menu/FilterMenuExample';
 import DividerExample from '@/components/dividers/DividerExample';
+import { Divider } from '@/components/dividers/Divider';
 
 export default function Home() {
   return (
     <main className="min-h-screen p-8">
 
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Accordion Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Accordion Component Examples</h2>
         <AccordionExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Advanced Menu Component Example</h2>
+        <h2 className="font-headline text-heading-md mb-6">Advanced Menu Component Example</h2>
         <AdvancedMenuExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Alert Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Alert Component Examples</h2>
         <AlertExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Basic Dialog Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Basic Dialog Component Examples</h2>
         <BasicDialogExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Button Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Button Component Examples</h2>
         <ButtonExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Checkbox Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Checkbox Component Examples</h2>
         <CheckboxItemExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Chip Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Chip Component Examples</h2>
         <ChipExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Dashboard Card Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Dashboard Card Component Examples</h2>
         <DashboardCardExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Data Table Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Data Table Component Examples</h2>
         <DataTableExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Divider Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Divider Component Examples</h2>
         <DividerExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Dropdown Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Dropdown Component Examples</h2>
         <DropdownSelectExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Filter Menu Component Example</h2>
+        <h2 className="font-headline text-heading-md mb-6">Filter Menu Component Example</h2>
         <FilterMenuExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Formatting Toolbar Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Formatting Toolbar Component Examples</h2>
         <FormattingToolbarExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Radio List Item Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Radio List Item Component Examples</h2>
         <RadioListExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Split Button Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Split Button Component Examples</h2>
         <SplitButtonExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">State Toggle Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">State Toggle Component Examples</h2>
         <StateToggleExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Stepper Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Stepper Component Examples</h2>
         <StepperExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Switch Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Switch Component Examples</h2>
         <SwitchExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Tab Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Tab Component Examples</h2>
         <TabsExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Text Field Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Text Field Component Examples</h2>
         <TextFieldExample />
       </section>
 
+      <div><Divider variant="horizontal" className="my-10"/></div>
+
       <section className="mt-8">
-        <h2 className="font-headline text-heading-sm mb-6">Tooltip Component Examples</h2>
+        <h2 className="font-headline text-heading-md mb-6">Tooltip Component Examples</h2>
         <TooltipExample />
       </section>
 
