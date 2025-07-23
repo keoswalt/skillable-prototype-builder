682fe2dbca92b661abd44c71e63803c9b781eae7
Kim Oswalt <130675196+keoswalt@users.noreply.github.com>
2025-07-23 13:25:06 -0500

    Update find and create menu options

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -8,17 +8,18 @@ export interface MenuItem {
 }
 
 export const FIND_MENU_ITEMS: MenuItem[] = [
-  { label: 'Lab instances', onClick: () => alert('Opens find lab instance page') },
-  { label: 'Lab profiles', onClick: () => alert('Opens find lab profile page') },
+  { label: 'Lab profile', onClick: () => alert('Opens find lab profile page') },
   { label: 'Lab series', onClick: () => alert('Opens find lab series page') },
-  { label: 'Organizations', onClick: () => alert('Opens find organizations page') },
-  { label: 'Users', onClick: () => alert('Opens find users page') },
+  { label: 'Lab instance', onClick: () => alert('Opens find lab instance page') },
+  { label: 'VM profile', onClick: () => alert('Opens find VM profile page') },
+  { label: 'User', onClick: () => alert('Opens find users page') },
   { label: 'Bug Report', onClick: () => alert('Opens find bug reports page') },
 ];
 
 export const CREATE_MENU_ITEMS: MenuItem[] = [
-  { label: 'New lab profile', onClick: () => alert('Opens template gallery to start lab profile creation') },
-  { label: 'New lab series', onClick: () => alert('Opens create lab series page') },
-  { label: 'New virtual machine', onClick: () => alert('Opens create new VM page') },
-  { label: 'New script template', onClick: () => alert('Opens create new script template page') },
+  { label: 'Create lab profile', onClick: () => alert('Opens template gallery to start lab profile creation') },
+  { label: 'Create lab series', onClick: () => alert('Opens create lab series page') },
+  { label: 'Create VM profile', onClick: () => alert('Opens create new VM page') },
+  { label: 'Create removable media', onClick: () => alert('Opens create removable media page') },
+  { label: 'Create script template', onClick: () => alert('Opens create new script template page') },
 ]; 
